b6a3a62aeb58a8187fe08a88b62f71a0f7631ad2
GodfreyAdiga <GodfreyAdiga@KEN-5CG0096HG5>
2022-11-28 14:26:40 +0300

    Functions

---
+++ src/main/java/FunctionalInterface/_Function.java
@@ -0,0 +1,16 @@
+package FunctionalInterface;
+
+public class _Function
+{
+
+    public static void main(String[] args)
+    {
+
+        int increment = increment(number);
+        System.out.println(increment);
+    }
+    static int increment(int number)
+    {
+        return number +1;
+    }
+}
