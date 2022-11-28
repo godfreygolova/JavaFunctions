2ac7c74b27eb10e70b761fde73cba70eaff87598
GodfreyAdiga <GodfreyAdiga@KEN-5CG0096HG5>
2022-11-28 14:51:58 +0300

    Merge remote-tracking branch 'origin/master'

--- src/main/java/FunctionalInterface/_Function.java
+++ src/main/java/FunctionalInterface/_Function.java
@@ -1,14 +1,18 @@
 package FunctionalInterface;
 
+import java.util.function.Function;
 public class _Function
 {
 
     public static void main(String[] args)
     {
 
-        int increment = increment(number);
+
+        int number = increment(number:1);
         System.out.println(increment);
     }
+    static Function<Integer, Integer> IncrementByOneFunction =
+            number -> number++;
     static int increment(int number)
     {
         return number +1;
